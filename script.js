fcfb643fb3226c205e6af4e0313807cdd60bb619
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-12-05 23:49:14 -0300

    Data Types.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lecture - Values and Variables
+
 var country = 'Brazil';
 var continent = 'South America';
 var population = 209500000;
@@ -5,3 +7,15 @@ var population = 209500000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+// Lecture - Data Types
+
+var isIsland = false;
+var language = 
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
