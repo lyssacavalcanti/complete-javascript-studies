51b4121707920c843ee1c0ae9875c6f751d76f48
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-12-13 20:11:46 -0300

    Equality Operators: == vs. ===

--- script.js
+++ script.js
@@ -127,3 +127,13 @@ console.log ('19' - '13'+ 17);
 console.log ('123'< 57);
 console.log (5 + 6 + '4'+ 9 - 4 - 2);
 
+// Equality Operators: == vs. ===
+var numNeighbours = 10;
+
+if (numNeighbours == 1) {
+    console.log ('Only 1 border.');
+} else if (numNeighbours > 1) {
+    console.log ('More than 1 border.')
+} else {
+    console.log ('No borders.')
+};
