ee7374519a49033f39426cf3f4e67806fa2afce4
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-08 10:35:21 -0300

    Calculated the averages of totals in the previous challenge

--- script.js
+++ script.js
@@ -401,3 +401,13 @@ totals.push(tipResult + bills[i]);
 };
 
 console.log(bills, tips, totals);
+
+const caclAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(caclAverage(totals));
